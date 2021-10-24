a272ec3fe3d67ff34f1b636582fa41269825c790
sato-s <s.sato.desu@gmail.com>
2021-10-24 17:10:07 +0900

    fin

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module fsnofity_config
 
 go 1.17
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/fsnotify/fsnotify v1.5.1
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"log"
+	"github.com/fsnotify/fsnotify"
 )
 
 type Config struct {
 	A        int
 	B        string
 	filename string
+	watcher  *fsnotify.Watcher
 }
 
 func NewConfig(filename string) (*Config, error) {
@@ -18,6 +21,7 @@ func NewConfig(filename string) (*Config, error) {
 	if err := config.loadFile(); err != nil {
 		return nil, err
 	}
+  go config.run()
 	return config, nil
 }
 
@@ -29,6 +33,36 @@ func (c *Config) loadFile() error {
 	err = yaml.Unmarshal([]byte(data), &c)
 	return err
 }
+
+func (c *Config) run() error {
+	watcher, err := fsnotify.NewWatcher()
+	defer watcher.Close()
+	if err != nil {
+		return err
+	}
+	if err := watcher.Add(c.filename); err != nil {
+		return err
+	}
+	log.Printf("Watching %s", c.filename)
+
+	for {
+		select {
+		case event := <-watcher.Events:
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				err := c.loadFile()
+				if err != nil {
+					log.Printf("Error: $s", err)
+				} else {
+					log.Printf("Refreshed setting from %s", c.filename)
+				}
+			}
+		case err := <-watcher.Errors:
+			log.Printf("Error: $s", err)
+		}
+	}
+	return nil
+}
+
 
 func main() {
 	config, err := NewConfig("config.yaml")
